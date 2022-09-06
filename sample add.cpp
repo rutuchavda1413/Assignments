62ff88d60be673fadc3ab2e1b61fe3add6653898
rutuchavda1413 <112864623+rutuchavda1413@users.noreply.github.com>
2022-09-06 12:16:34 +0600

    c++

---
+++ sample add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{ 
+	int num1,num2;
+	int ans;
+	
+	cout<<"\n Enter Number : ";
+	cin>>num1;
+	
+	cout<<"\n Enter Number : ";
+	cin>>num2;
+	
+	ans=num1+num2;
+	
+	cout<<"\n Ans="<<ans;
+}
