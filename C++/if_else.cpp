2474a023e09bf0c7365e83d8482a5fe791f09d46
rutuchavda1413 <112864623+rutuchavda1413@users.noreply.github.com>
2022-09-06 12:40:53 +0600

    save

---
+++ C++/if_else.cpp
@@ -0,0 +1,18 @@
+// IF..ELSE CONDITION
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	num=50;
+	
+	if(num>35)
+	{
+		cout<<"\n You Are Pass";
+	}
+	else
+	{
+		cout<<"\n You Are Fail";
+	}
+
+}
